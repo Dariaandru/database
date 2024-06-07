f4c978bae3764e459f75bcdc72ded9233031a367
Dariaandru <147586572+Dariaandru@users.noreply.github.com>
2024-06-07 22:12:39 +0700

    пока все хорошо....
    работает страница регистрации, данные добавляются в базу данных в таблицу, есть кнопки регистрации на главной странице

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Welcome to My Website</h1>
+        <p>Please choose an option:</p>
+        <a href="login.php" class="button">Login</a>
+        <a href="register.php" class="button">Register</a>
+    </main>
+    <footer>
+        <p>&copy; 2022 My Website</p>
+    </footer>
+</html>
